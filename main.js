c9cce3f53f6fe8229e14c2b3cf6913a969c0d41c
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-04-23 22:00:36 -0700

    thresholding works now

--- main.js
+++ main.js
@@ -9,25 +9,34 @@ var rotation = 0;
 var rotation_step = 90;
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (typeof navigator.bluetooth == "undefined"){
-        error("WebBluetooth is not supported in this browser.")
-    }
-    $("#connect-btn").addEventListener("click", (event) => {
-        connect_printer();
-    })
-    $("#print-btn").addEventListener("click", (event) => {
-        print_image($("#image-canvas"));
-    })
-    $("#image-file").addEventListener("change", (event) => {
-        rotation = 0;
-        draw_image();
-    })
+    var connect_btn = $("#connect-btn");
+    var print_btn = $("#print-btn");
     $("#left-btn").addEventListener("click", (event) => {
         rotate_left();
     });
     $("#right-btn").addEventListener("click", (event) => {
         rotate_right();
     });
+    document.querySelectorAll("input").forEach((input) => {
+        input.addEventListener("change", (event) => {
+            if(event.target == $("#image-file")){
+                rotation = 0;
+            }
+            draw_image();
+        });
+    });
+    if (typeof navigator.bluetooth == "undefined"){
+        error("WebBluetooth is not supported in this browser.");
+        connect_btn.style.cursor = "not-allowed";
+        print_btn.style.cursor = "not-allowed";
+    }else{
+        connect_btn.addEventListener("click", (event) => {
+            connect_printer();
+        })
+        print_btn.addEventListener("click", (event) => {
+            print_image($("#image-canvas"));
+        })
+    }
 })
 
 function draw_image(){
@@ -43,7 +52,15 @@ function draw_image(){
             var ratio = image.height/image.width;
             var scaled_height = canvas.width*ratio;
             canvas.height = scaled_height;
-            context.drawImage(image, 0, 0, canvas.width, scaled_height);
+            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+            var dither = $("input[type='radio']:checked").value;
+            var image_data = context.getImageData(0, 0, canvas.width, canvas.height);
+            if(dither == "dither-threshold"){
+                threshold(image_data, $("#threshold-val").value);
+            }else{
+                return;
+            }
+            context.putImageData(image_data, 0, 0);
         };
         get_rotated_image_data_url(e.target.result, rotation).then((data_url) => {
             image.src = data_url;
@@ -84,4 +101,12 @@ function rotate_left(){
 function rotate_right(){
     rotation = (rotation+rotation_step)%360;
     draw_image();
+}
+
+function threshold(image_data, threshold) {
+    var data = image_data.data;
+    for (let i = 0; i < data.length; i += 4) {
+        var v = data[i] < threshold ? 0 : 255;
+        data[i] = data[i + 1] = data[i + 2] = v;
+    }
 }
